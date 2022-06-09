747e87117258e5d995fd00bfd8800451d7719194
goose-cba <danvi.cba@gmail.com>
2022-06-09 20:58:10 -0300

    función potencia
    Se utiliza la función pow para obtener la potencia de un numero, se realizan pruebas de escritorio.

--- Carpeta Principal/Ejercicio6.py
+++ Carpeta Principal/Ejercicio6.py
@@ -1,1 +1,21 @@
+"""
+6- función potencia. Retorna la potencia del primero elevado al segundo parametros
+
+-[ ] Implementar la funcion
+-[ ] Pull Request
+
+-------------------------------------------
+                 PRUEBAS
+-------------------------------------------
+- positivo x, positivo Y -->  pow (2, 2)
+- negativo x, positivo y -->  pow (-2, 2) 
+- positivo x, negativo y -->  pow (2, -2) 
+- negativo x, negativo y -->  pow (-2, -2)
+
+"""
+
+def potencia(a, b):
+  resultado = pow(a, b)
+  print(resultado)
+
 
