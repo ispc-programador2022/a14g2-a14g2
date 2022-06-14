435be7737cc7f2d430afa3286499a835cc924e19
clau-yon <clau.yon.15@mi.unc.edu.ar>
2022-06-14 05:55:38 -0300

    Radicacion

--- Carpeta Principal/Ejercicio7.py
+++ Carpeta Principal/Ejercicio7.py
@@ -1,1 +1,13 @@
+'''
+Funcion Radicacion, devuelve la raiz del primer respecto del segundo parametro
+usando la funcion pow()
+'''
 
+def radicacion(x,y):
+    z=pow(x,1/y)
+    return z
+
+#ejemplo
+x=int(input("Ingrese un valor de x: "))
+y=int(input("Ingrese un valor de y: "))
+print(radicacion(x,y))
